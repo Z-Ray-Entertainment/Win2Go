a8e857e46ed99c23cb84de5429d8f0b9ae2c3400
Vortex Acherontic <vortex@z-ray.de>
2025-09-09 22:15:00 +0200

    Fixed register gresource

--- win2go/__init__.py
+++ win2go/__init__.py
@@ -4,11 +4,14 @@ import sys
 
 from os import environ
 
-from gi.repository.Gio import Resource
+from gi.repository import Gio
 
 pkgdatadir = '@pkgdatadir@'
 if environ.get("FLATPAK_ID") is not None:
     sys.path.insert(1, pkgdatadir)
+
+resource = Gio.Resource.load(filename=os.path.join(pkgdatadir, "win2go.gresource"))
+Gio.resources_register(resource)
 
 from win2go.const import LOCALE_DIR, APP_ID
 from win2go.win2go_app import Win2Go
@@ -18,12 +21,8 @@ gettext.install('win2go', LOCALE_DIR)
 
 def run():
     print("Works!")
-    Win2Go(application_id=APP_ID).run(sys.argv)
+    sys.exit(Win2Go(application_id=APP_ID).run(sys.argv))
 
 
 if __name__ == '__main__':
-    from gi.repository import Gio
-
-    resource: Resource = Gio.Resource.load(filename=os.path.join(pkgdatadir, "win2go.gresource"))
-    resource.register()
     run()
--- win2go/const.py
+++ win2go/const.py
@@ -7,4 +7,4 @@ else:
     VERSION = "0.dev0"
     LOCALE_DIR = "data/po"
 APP_NAME = "Win2Go"
-APP_ID = "de.z_ray.Win2G0"
+APP_ID = "@APPID@"
