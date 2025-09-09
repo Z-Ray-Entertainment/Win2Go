573ab182abde6ac2fe886d169ef693e8ab28d173
Vortex Acherontic <vortex@z-ray.de>
2025-09-09 22:38:50 +0200

    Fix main window

--- win2go/ui/main_window.py
+++ win2go/ui/main_window.py
@@ -4,5 +4,5 @@ gi.require_version("Adw", "1")
 from gi.repository import Gtk, Adw, Gio
 
 @Gtk.Template(resource_path="/de/z_ray/win2go/blp/main_window.ui")
-class MainWindow(Adw.ApplicationWindow):
+class MainWindow(Gtk.ApplicationWindow):
     __gtype_name__ = "Win2GoMainWindow"
