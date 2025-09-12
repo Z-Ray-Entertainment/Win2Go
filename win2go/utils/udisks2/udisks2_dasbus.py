0cd841a3ac06d3f6d188f29b4a500d8f52b3ef7c
Vortex Acherontic <vortex@z-ray.de>
2025-09-12 21:27:53 +0200

    Remove pydbus based udisks2

--- win2go/utils/udisks2/udisks2_dasbus.py
+++ win2go/utils/udisks2/udisks2_dasbus.py
@@ -1,14 +1,19 @@
+import os
 from typing import List
 
 from dasbus.connection import SystemMessageBus
 from dasbus.error import DBusError
+from dasbus.unix import GLibClientUnix
+from gi.overrides import GLib
+from gi.repository.Gio import File
 
 from win2go.utils.udisks2.block_device import BlockDevice
 
 sys_bus = SystemMessageBus()
 
 def get_supported_filesystems() -> List[str]:
-    proxy = sys_bus.get_proxy("org.freedesktop.UDisks2", "/org/freedesktop/UDisks2/Manager")
+    proxy = sys_bus.get_proxy("org.freedesktop.UDisks2",
+                              "/org/freedesktop/UDisks2/Manager")
     print(proxy.SupportedFilesystems)
     return proxy.SupportedFilesystems
 
@@ -16,13 +21,17 @@ def is_ntfs_supported() -> bool:
     return "ntfs" in get_supported_filesystems()
 
 def get_managed_objects() -> List[str]:
-    proxy = sys_bus.get_proxy("org.freedesktop.UDisks2", "/org/freedesktop/UDisks2", "org.freedesktop.DBus.ObjectManager")
+    proxy = sys_bus.get_proxy("org.freedesktop.UDisks2",
+                              "/org/freedesktop/UDisks2",
+                              "org.freedesktop.DBus.ObjectManager")
     return proxy.GetManagedObjects()
 
 def find_removable_media() -> List[BlockDevice]:
     devices_found = []
     for managed in get_managed_objects():
-        managed_properties = sys_bus.get_proxy("org.freedesktop.UDisks2", managed, "org.freedesktop.DBus.Properties")
+        managed_properties = sys_bus.get_proxy("org.freedesktop.UDisks2",
+                                               managed,
+                                               "org.freedesktop.DBus.Properties")
         try:
             if managed_properties.Get("org.freedesktop.UDisks2.Drive", "Removable"):
                 model = managed_properties.Get("org.freedesktop.UDisks2.Drive", "Model")
@@ -32,3 +41,15 @@ def find_removable_media() -> List[BlockDevice]:
         except DBusError:  # Not all Devices have the .Drive Interface (e.g. loop devices)
             pass
     return devices_found
+
+def mount_iso_image(file: File):
+    proxy = sys_bus.get_proxy("org.freedesktop.UDisks2",
+                              "/org/freedesktop/UDisks2/Manager",
+                              "org.freedesktop.UDisks2.Manager",
+                              client=GLibClientUnix)
+
+    fd = os.open(file.get_path(), os.O_RDONLY)
+    auth = GLib.Variant.new_byte(False)
+    readonly = GLib.Variant.new_byte(True)
+    fd_variant = GLib.Variant.new_handle(fd)
+    proxy.LoopSetup(0, {"fd": fd_variant, "auth.no_user_interaction": auth, "read-only": readonly, },)
