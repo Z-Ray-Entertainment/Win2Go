f4b9ad1b86a6beda7a3885e25a8b2cf419b19a25
Vortex Acherontic <vortex@z-ray.de>
2025-09-15 21:51:46 +0200

    (wip) creating windows boot entry

--- win2go/utils/bcdboot/bcdboot.py
+++ win2go/utils/bcdboot/bcdboot.py
@@ -32,13 +32,37 @@ def _copy_bootmgr(boot_mount_path: str, windows_mount_path: str):
 
 
 def _build_bcd_store(boot_mount_path: str):
-    print("Building bcd store...")
-    bsc_src = "{share}/win2go/bcd/BCD".format(share=SHARE_DIR)
-    boot_reg_src = "{share}/win2go/bcd/boot.reg".format(share=SHARE_DIR)
+    print("Setup BCD...")
 
-    bsc_dst = "{boot}/EFI/Microsoft/Boot/BCD".format(boot=boot_mount_path)
-    shutil.copy(bsc_src, bsc_dst)
-
-    subprocess.run(["hivexregedit", "--merge", "--prefix", "BCD00000001", bsc_dst, boot_reg_src])
+    bcd_path = _copy_bcd(boot_mount_path)
+    _patch_bcd(bcd_path, boot_mount_path)
+    winload_path = _extract_winload(bcd_path, boot_mount_path)
+    _create_windows_boot_entry(bcd_path, winload_path)
 
     print("Done building bcd store")
+
+def _copy_bcd(boot_mount_path: str) -> str:
+    print("Copying BCD...")
+
+    bsc_src = "{share}/win2go/bcd/BCD".format(share=SHARE_DIR)
+    bcd_dst = "{boot}/EFI/Microsoft/Boot/BCD".format(boot=boot_mount_path)
+    shutil.copy(bsc_src, bcd_dst)
+
+    print("BCD created at {}".format(bcd_dst))
+    return bcd_dst
+
+def _patch_bcd(bcd_path: str, boot_path: str):
+    print("Patching BCD...")
+    boot_reg_src = "{share}/win2go/bcd/boot.reg".format(share=SHARE_DIR)
+    subprocess.run(["hivexregedit", "--merge", "--prefix", "BCD00000001", bcd_path, boot_reg_src])
+
+def _extract_winload(bcd_path: str, boot_path: str) -> str:
+    print("Extract winload info from BCD...")
+    winload_dst = "{boot_path}/winload.txt".format(boot_path=boot_path)
+    subprocess.run(["hivexsh", "-w", bcd_path, "-f", winload_dst])
+
+    print("Winload extracted to {}".format(winload_dst))
+    return winload_dst
+
+def _create_windows_boot_entry(bcd_path: str, winload_path: str):
+    print("Creating windows boot entry...")
