19f35b162b350d1f644404b91cd0a8c0b14495e2
Vortex Acherontic <vortex@z-ray.de>
2025-09-15 20:37:10 +0200

    copy bootmgr to BOOT

--- win2go/utils/bcdboot/bcdboot.py
+++ win2go/utils/bcdboot/bcdboot.py
@@ -1,10 +1,29 @@
 import os
+import shutil
 
 
-async def create_bootloader(boot_mount_path: str, windows_install_path: str):
+async def create_bootloader(boot_mount_path: str, windows_mount_path: str):
     print("Creating bootloader...")
-    #_copy_bootmgr(boot_mount_path, windows_install_path)
+    if boot_mount_path != "" and windows_mount_path != "":
+        _copy_bootmgr(boot_mount_path, windows_mount_path)
+    else:
+        print("No boot mount path or windows mount path")
 
-def _copy_bootmgr(boot_mount_path: str, windows_install_path: str):
+
+def _copy_bootmgr(boot_mount_path: str, windows_mount_path: str):
+    print("Copying bootmgr...")
     recovery_path = "{boot}/EFI/Microsoft/Recovery".format(boot=boot_mount_path)
-    os.makedirs(recovery_path)
+    os.makedirs(recovery_path, exist_ok=True)
+
+    efi_src = "{winmnt}/Windows/Boot/EFI".format(winmnt=windows_mount_path)
+    boot_dst = "{bootmnt}/EFI/Microsoft/Boot".format(bootmnt=boot_mount_path)
+    shutil.copytree(efi_src, boot_dst, dirs_exist_ok=True)
+
+    font_src = "{winmnt}/Windows/Boot/Fonts".format(winmnt=windows_mount_path)
+    shutil.copytree(font_src, boot_dst, dirs_exist_ok=True)
+
+    res_src = "{winmnt}/Windows/Boot/Resources".format(winmnt=windows_mount_path)
+    if os.path.exists(res_src):
+        shutil.copytree(res_src, boot_dst, dirs_exist_ok=True)
+
+    print("Done copying bootmgr")
